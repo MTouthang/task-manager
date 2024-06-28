ff5a58bb0c23ff2fddbfc017249e59aef0132d92
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-29 00:02:23 +0530

    feat: register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,47 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validate field
+  if ((!name || !email, !password)) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  // check if user exist
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Exists");
+  }
+
+  // hashed password and create user
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create([
+    {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  ]);
+
+  //   check if user is created
+  if (user) {
+    res.status(201).json({
+      user,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
   res.json({ message: "Register User successful" });
 });
 
