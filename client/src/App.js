463353315033433a0eb496b2daa43a9bcf582aab
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-07-07 13:24:50 +0530

    feat: registe page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-
-import { Counter } from "./features/counter/Counter";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Header from "./components/Header";
 function App() {
-  return <p> hello world </p>;
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
